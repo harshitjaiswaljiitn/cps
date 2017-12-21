617256abccdae32e8d64dca148fc5744178b9275
Harshit Jaiswal <33958688+sylvestercodivator@users.noreply.github.com>
2017-12-21 18:17:21 +0000

    Add files via upload

---
+++ pangram.cpp
@@ -0,0 +1,37 @@
+/*
+Question Link : 
+
+https://www.hackerrank.com/challenges/pangrams/problem
+
+*/
+#include<stdlib.h>
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+	char s[1000];
+	int a[200];
+	int c=0;
+	for(int i=97;i<123;i++)
+		a[i]=0;
+		
+	scanf("%[^\n]s",&s);
+	int len=strlen(s);
+	//cout<<len;
+	for(int i=0;i<len;i++)
+	{
+		int k=(char)tolower(s[i]);
+		a[k]++;
+	}
+	
+	for(int i=97;i<123;i++)
+	{
+		if(a[i]==0)
+			c++;
+	}
+	if(c==0)
+		cout<<"pangram";
+	else
+		cout<<"not pangram";
+}a
